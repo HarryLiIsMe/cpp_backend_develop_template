b0a652df2cb073c36a50942a20d418b3173eea6e
root <root@gmail.com>
2024-08-08 03:25:44 +0800

    update make lint

--- cmake/lint.cmake
+++ cmake/lint.cmake
@@ -1,15 +1,22 @@
 # make lint
+# https://clang.llvm.org/extra/clang-tidy/checks/list.html
+# https://clang.llvm.org/extra/clang-tidy/index.html
 # clang-tidy version >= 18.1.8
-# clang-tidy --checks="-llvmlibc-*,-modernize-use-trailing-return-type,-readability-identifier-length,-performance-avoid-endl,-cppcoreguidelines-avoid-magic-numbers,-readability-named-parameter" --format-style=google --dump-config > .clang-tidy
-# clang-tidy --checks="-*,clang-diagnostic-*,clang-analyzer-*,cppcoreguidelines-*,google-*,-google-readability-*" --warnings-as-errors=* --format-style=google --dump-config > .clang-tidy
+# clang-tidy --checks=*,-* --warnings-as-errors=* --format-style=google --dump-config > .clang-tidy
 add_custom_target(lint
     COMMAND clang-tidy --config-file=.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${PROJECT_BINARY_DIR} 
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMENT "lint code"
 )
+# add_custom_target(lint
+#     COMMAND clang-tidy --config-file=.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${CMAKE_BINARY_DIR} 
+#     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+#     COMMENT "lint code"
+# )
 
 # set(CLANG_TIDY_COMMAND)
 # set(CMAKE_CXX_CLANG_TIDY)
+
 
 # add_custom_target(lint
 # COMMAND cppcheck --check-config .cppcheck -I ./inc --enable=all --inconclusive --suppress=missingIncludeSystem --std=c++11 ${SRC_FILES} ${TEST_FILES}
