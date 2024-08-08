0ab9fc436d30371d6880d72a034600aa5853f84f
root <root@gmail.com>
2024-08-08 19:27:05 +0800

    fmt and lint complete config file path

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,10 +1,11 @@
 # make fmt
+# https://github.com/llvm/llvm-project.git
 # https://clang.llvm.org/docs/ClangFormat.html
 # https://clang.llvm.org/docs/ClangFormatStyleOptions.html
 # clang-format version >= 18.1.8
 # clang-format --style=Google --dump-config > .clang-format
 add_custom_target(fmt
-    COMMAND clang-format -i ${SRC_FILES} ${INC_FILES} ${TEST_FILES}
+    COMMAND clang-format --style=file:${CMAKE_SOURCE_DIR}/.clang-format -i ${SRC_FILES} ${INC_FILES} ${TEST_FILES}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMENT "fmt code"
 )
--- cmake/lint.cmake
+++ cmake/lint.cmake
@@ -1,20 +1,21 @@
 # make lint
+# https://github.com/llvm/llvm-project.git
 # https://clang.llvm.org/extra/clang-tidy/checks/list.html
 # https://clang.llvm.org/extra/clang-tidy/index.html
 # clang-tidy version >= 18.1.8
 # clang-tidy --checks=*,-* --warnings-as-errors=* --format-style=google --dump-config > .clang-tidy
 add_custom_target(lint
-    COMMAND clang-tidy --config-file=.clang-tidy ${SRC_FILES} ${TEST_FILES} -- -x c++ -I ${INC_DIRS} -I ${GTEST_INCS}
+    COMMAND clang-tidy --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy ${SRC_FILES} ${TEST_FILES} -- -x c++ -I ${INC_DIRS} -I ${GTEST_INCS}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMENT "lint code"
 )
 # add_custom_target(lint
-#     COMMAND clang-tidy --config-file=.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${PROJECT_BINARY_DIR} 
+#     COMMAND clang-tidy --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${PROJECT_BINARY_DIR} 
 #     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 #     COMMENT "lint code"
 # )
 # add_custom_target(lint
-#     COMMAND clang-tidy --config-file=.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${CMAKE_BINARY_DIR} 
+#     COMMAND clang-tidy --config-file=${CMAKE_SOURCE_DIR}/.clang-tidy ${SRC_FILES} ${TEST_FILES} -p ${CMAKE_BINARY_DIR} 
 #     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 #     COMMENT "lint code"
 # )
@@ -23,8 +24,11 @@ add_custom_target(lint
 # set(CMAKE_CXX_CLANG_TIDY)
 
 
+# https://github.com/danmar/cppcheck
+# https://sourceforge.net/p/cppcheck/wiki/ListOfChecks/
+# https://cppcheck.sourceforge.io/manual.pdf
 # add_custom_target(lint
-# COMMAND cppcheck --check-config .cppcheck -I ./inc --enable=all --inconclusive --suppress=missingIncludeSystem --std=c++11 ${SRC_FILES} ${TEST_FILES}
+# COMMAND cppcheck -I ./inc --enable=all --inconclusive --suppress=missingIncludeSystem --std=c++11 ${SRC_FILES} ${TEST_FILES}
 # WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 # COMMENT "lint code"
 # )
