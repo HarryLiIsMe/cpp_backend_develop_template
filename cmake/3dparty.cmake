a27fa72b3698feddab4640afce183e527d3b0f44
root <root@gmail.com>
2024-12-01 01:14:39 +0800

    add project_name

---
+++ cmake/3dparty.cmake
@@ -0,0 +1,10 @@
+set(3DPARTY_SRC)
+
+# add dotenv-c
+set(DOTENV_DIRS ${3DPARTY}/dotenv-c-0.1.0)
+set(DOTENV_SRC_DIRS ${DOTENV_DIRS}/src)
+set(DOTENV_INCS ${DOTENV_DIRS}/src)
+add_subdirectory(${DOTENV_DIRS})
+include_directories(${DOTENV_INCS})
+file(GLOB DOTENV_SRC "${DOTENV_SRC_DIRS}/*.c")
+list(APPEND 3DPARTY_SRC ${DOTENV_SRC})
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
+#include <cstdlib>
 #include <iostream>
 
 #include "common.h"
+#include "dotenv.h"
 #include "errmaner.h"
 #include "logmaner.h"
 #include "vermaner.h"
+
+using namespace std;
+using std::cout, std::endl, std::string, std::getenv;
 
 i32_t main(void) {
   ErrorManer::instance();
   VerManer::instance();
   LogManer::instance();
 
-  std::cout << VerManer::instance().getBuildDateTime()
-            << " version: " << VerManer::instance().getCurrVer() << std::endl;
+  env_load(".", false);
+  const string project_name = getenv("PROJECT_NAME");
+
+  cout << "hello world " << project_name << endl;
+  cout << VerManer::instance().getBuildDateTime()
+       << " version: " << VerManer::instance().getCurrVer() << endl;
   return 0;
 }
