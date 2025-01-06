b00b850002d0e4362db8e02e2568a0688038b4ff
HarryLiIsMe <42673017+HarryLiIsMe@users.noreply.github.com>
2025-01-06 16:57:38 +0800

    Update test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(TestCaseSuite2, test_case1) { ASSERT_TRUE(true) << "test case1 failed"; }
+TEST(TestCaseSuite2, test_case3) { ASSERT_TRUE(true) << "test case1 failed"; }
 
-TEST(TestCaseSuite2, test_case2) { ASSERT_FALSE(true) << "test case2 failed"; }
+TEST(TestCaseSuite2, test_case4) { ASSERT_FALSE(true) << "test case2 failed"; }
